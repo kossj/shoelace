00f3bd812ec7403ec77226df360fa3955fcd6215
= <=>
2023-10-10 21:45:11 -0500

    extracted js to seperate file

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
-var pointa = {x:0,y:0};
-var pointb = {x:2,y:0};
-var pointc = {x:0,y:2};
-
-var points = [pointa,pointb,pointc]
-
-function shoelace(points) {
-points.push(points[0])
-
-var xypairs=0, yxpairs=0;
-
-for (i=0;i<points.length-1;i++) {
-    xypairs += (points[i].x * points[i+1].y)
-    yxpairs += (points[i].y * points[i+1].x)
+function createPoint() {
+    point = document.createElement("div");
+    point.innerHTML = '<input id="x" type="number"><input id="y" type="number">';
+    document.getElementById("points").append(point);
 }
 
-area = 0.5 * (xypairs-yxpairs)
+function shoelace(points) {
+    points.push(points[0])
 
-return area
+    var xypairs = 0, yxpairs = 0;
+
+    for (i = 0; i < points.length - 1; i++) {
+        xypairs += (points[i].x * points[i + 1].y)
+        yxpairs += (points[i].y * points[i + 1].x)
+    }
+
+    area = 0.5 * (xypairs - yxpairs)
+
+    return area
+}
+
+function doShoelace() {
+    children = document.getElementById("points").children;
+    points = [];
+
+    for (i = 0; i < children.length; i++) {
+        var x = parseFloat(children[i].querySelector("#x").value);
+        var y = parseFloat(children[i].querySelector("#y").value);
+
+        points.push({ x: x, y: y });
+    }
+
+    r = shoelace(points);
+
+    document.getElementById("areaButton").value = r;
+
 }
